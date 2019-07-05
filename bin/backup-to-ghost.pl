51f1faa26872aa9bd6bb73ad946474ae514deee8
Trieu Vu <trieuv@strategicdata.com.au>
2019-07-05 16:15:25 +1000

    export backup.yml into an encoded JSON object for bulk POST

--- bin/backup-to-ghost.pl
+++ bin/backup-to-ghost.pl
@@ -9,8 +9,77 @@ use YAML::XS;
 use Data::Structure::Util qw( unbless );
 use Encode qw( decode_utf8 );
 use Data::Dumper;
+use Cpanel::JSON::XS;;
 
 # $YAML::XS::UseCode=1;
 
 my $yaml = read_file('data/backup.yml');
 my @array = Load $yaml;
+
+my %tag;
+foreach my $item (@array) {
+    next unless
+        ref $item eq 'ruby/object:Tag'
+        && $item->{attributes}{taggable_type} eq 'CanpubArticle'
+        && $item->{attributes}{phrase} ne 'sidebar'
+        && $item->{attributes}{slug} ne 'in-the-media1';
+
+    #if (exists $tag{$item->{attributes}{taggable_id}}) {
+    #    warn Dumper $tag{$item->{attributes}{taggable_id}};
+    #    warn Dumper $item;
+    #}
+
+    push @{$tag{$item->{attributes}{taggable_id}}},
+         $item->{attributes}{slug};
+}
+
+my %types;
+map { $types{ref $_}++ } @array;
+#print Dumper \%types;
+
+# posts is an array, so we can prepare everythin
+# and dump it in the array and make one big POST
+#{
+#    "posts": [{
+#        "title": "My test post",
+#        "tags": [{"name": "my tag", "description": "a very useful tag"}, {"name": "#hidden"}],
+#        "authors": [{"id": "5c739b7c8a59a6c8ddc164a1"}, {"id": "5c739b7c8a59a6c8ddc162c5"}, {"id": "5c739b7c8a59a6c8ddc167d9"}]
+#    }]
+#}
+
+my @content_ref;
+foreach my $item (@array) {
+
+    next unless ref $item eq 'ruby/object:CanpubArticle';
+
+    my $created = $item->{attributes}{created_on};
+    $created =~ s/(\d\d\d\d-\d\d-\d\d)/$1/;
+    my $published = $item->{attributes}{publish_on} || $created;
+
+    my $content = decode_utf8($item->{attributes}{content_markup});
+    $content =~ s/^\s*|\s*$//g;
+
+    my $post = {
+        categories => defined $tag{$item->{attributes}{id}}
+                        ? join " ", @{$tag{$item->{attributes}{id}}}
+                        : undef,
+        assets     => $item->{assets},
+        link       => $item->{attributes}{link},
+        published  => $published,
+        title      => $item->{attributes}{title},
+        # _blueprint => $item->{attributes},
+        content    => $content,
+    };
+
+    if ( $item->{attributes}{excerpt} ) {
+        my $excerpt = decode_utf8($item->{attributes}{excerpt});
+        $excerpt =~ s/\r\n/ /gs;
+        $post->{excerpt} = $excerpt;
+    }
+
+    push @content_ref, $post;
+}
+
+my $encoded_content = Cpanel::JSON::XS->new->allow_blessed({posts => \@content_ref});
+print $encoded_content . "\n";
+
